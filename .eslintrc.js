eef24c82a80511c2783f6fb7e012ce76d6d649b5
answer-zf <feng18255163789@gmail.com>
2019-11-18 17:10:46 +0800

    home

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,20 @@
 module.exports = {
-  root: true,
   env: {
-    es6: true,
-    node: true
+    browser: true,
+    node: true,
+    es6: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
+  plugins: ['vue'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    //强制使用单引号
-    quotes: ['error', 'single'],
-    //强制不使用分号结尾
-    semi: ['error', 'never'],
-    // 关闭冲突规则
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'none',
-        bracketSpacing: true,
-        jsxBracketSameLine: true,
-        parser: 'flow'
-      }
-    ]
+    'no-irregular-whitespace': 'off'
   }
 }
-// npm install --save-dev eslint-plugin-prettier eslint-config-prettier prettier-eslint-cli
